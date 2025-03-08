351b846e9fc5257d58226a7fa9879bfaf7049b5e
ValT78 <118465560+ValT78@users.noreply.github.com>
2025-03-08 16:41:12 +0100

    real food

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,13 @@ mod food;
 use creature::CreaturePlugin;
 use simulation_speed::SimulationSpeedPlugin;
 use player_camera::PlayerCameraPlugin;
+use food::FoodPlugin;
 
 const MAP_WIDTH: i32 = 256;
 const MAP_HEIGHT: i32 = 256;
 
 fn main() -> AppExit{
     App::new()
-    .add_plugins((DefaultPlugins, CreaturePlugin, SimulationSpeedPlugin, PlayerCameraPlugin))
+    .add_plugins((DefaultPlugins, CreaturePlugin, SimulationSpeedPlugin, PlayerCameraPlugin, FoodPlugin))
     .run()
 }
